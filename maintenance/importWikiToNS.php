3f8c789b1058ffda8a32781198534220ad44fa6b
Mark A. Hershberger <mah@nichework.com>
2017-08-24 17:01:49 -0400

    Fix up MW Style errors
    Couple of potential bug fixes, too

--- maintenance/importWikiToNS.php
+++ maintenance/importWikiToNS.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Copyright (C) 2017  Mark A. Hershberger
  *
@@ -20,45 +19,45 @@
 namespace ImportWikiToNS;
 
 $maintPath = ( getenv( 'MW_INSTALL_PATH' ) !== false
-              ? getenv( 'MW_INSTALL_PATH' )
-              : __DIR__ . '/../../..' ) . '/maintenance/Maintenance.php';
-if ( !file_exists( $maintPath ) ) {;
-    echo "Please set the environment variable MW_INSTALL_PATH "
-        . "to your MediaWiki installation.";
-    exit 1;
+			  ? getenv( 'MW_INSTALL_PATH' )
+			  : __DIR__ . '/../../..' ) . '/maintenance/Maintenance.php';
+if ( !file_exists( $maintPath ) ) {
+	echo "Please set the environment variable MW_INSTALL_PATH "
+		. "to your MediaWiki installation.";
+	exit( 1 );
 }
 require_once $maintPath;
 
 class Import extends Maintenance {
-    public function __construct() {
-        parent::__construct();
+	public function __construct() {
+		parent::__construct();
 
-        $this->addDescription( "Import a wiki's dump file into your main namespace." );
-        $this->addOption( "n", "A comma separted list of namespaces in the import "
-                          . "to import other than the main NS.", false, true,
-                          "namespaces" );
-        $this->addOption( "t", "The target namespace to import the main ns in the "
-                          . "import to. Links in the import to pages in the main "
-                          . "namespace will be translated to this namespace.",
-                          true, true, "target" );
-        $this->addOption( "i", "The integer value of the target namespace.  If this "
-                          . "is not given, the script will use your configuration "
-                          . "to determine this and error out if none is found.",
-                          false, true, "targetID" );
-        $this->addOption( "s", "The value of \$wgServer for the source wiki that "
-                          . "the dump originates on. This will be used for file "
-                          . "imports.", false, true, "wgServer" );
-        $this->addOption( "M", "Attempt to run with default memory limits.  This is "
-                          . "likely to cause problems with the import, but you can try.",
-                          false, false, "limit-memory" );
-        $this->addArg( "dumpfile", "The XML export to use. STDIN is used if this "
-                       . "isn't given.", false );
-        $this->addArg( "outfile", "Where to save the XML output.  STDOUT is used if this "
-                       . "isn't given.", false );
-    }
+		$this->addDescription( "Import a wiki's dump file into your main namespace." );
+		$this->addOption( "n", "A comma separted list of namespaces in the import "
+						  . "to import other than the main NS.", false, true,
+						  "namespaces" );
+		$this->addOption( "t", "The target namespace to import the main ns in the "
+						  . "import to. Links in the import to pages in the main "
+						  . "namespace will be translated to this namespace.",
+						  true, true, "target" );
+		$this->addOption( "i", "The integer value of the target namespace.  If this "
+						  . "is not given, the script will use your configuration "
+						  . "to determine this and error out if none is found.",
+						  false, true, "targetID" );
+		$this->addOption( "s", "The value of \$wgServer for the source wiki that "
+						  . "the dump originates on. This will be used for file "
+						  . "imports.", false, true, "wgServer" );
+		$this->addOption( "M", "Attempt to run with default memory limits.  This is "
+						  . "likely to cause problems with the import, but you can try.",
+						  false, false, "limit-memory" );
+		$this->addArg( "dumpfile", "The XML export to use. STDIN is used if this "
+					   . "isn't given.", false );
+		$this->addArg( "outfile", "Where to save the XML output.  STDOUT is used if this "
+					   . "isn't given.", false );
+	}
 
-    public function execute() {
-    }
+	public function execute() {
+	}
 }
 
 $maintClass = "ImportWikiToNS\\Import";
--- src/MWStreamFilter.php
+++ src/MWStreamFilter.php
@@ -17,234 +17,319 @@
  */
 
 class MWStreamFilter extends XMLWritingIteration {
-    private $re;
-    private $ns;
-    private $nsID;
-    private $oldCase;
+	private $re;
+	private $ns;
+	private $nsID;
+	private $oldCase;
 
-    private $inName;
-    private $outName;
+	private $inName;
+	private $outName;
 
-    private $title;
-    private $content;
+	private $title;
+	private $content;
 
-    private $titles;
+	private $titles;
 
-    function __construct( $inFile, $outFile ) {
-        $inTitle = new XMLReader;
-        $inTitle->open($inFile);
+	/**
+	 * Construct!
+	 * @param string $inFile filename to read
+	 * @param string $outFile filename to write
+	 */
+	public function __construct( $inFile, $outFile ) {
+		$inTitle = new XMLReader;
+		$inTitle->open( $inFile );
+		// Read input twice.  Got to cache the pages the first time through.
+		$this->getPageTitles( $inTitle );
 
-        $in = new XMLReader;
-        $in->open($inFile);
-        $this->inName = $inFile;
-        // Read input twice.  Got to cache the pages the first time through.
-        $this->getPageTitles( $inTitle );
+		$in = new XMLReader;
+		$in->open( $inFile );
+		$this->inName = $inFile;
 
-        $out = new XMLWriter();
-        $out->openURI($outFile);
-        $this->outName = $inFile;
+		$out = new XMLWriter();
+		$out->openURI( $outFile );
+		$this->outName = $inFile;
 
+		parent::__construct( $out, $in );
+	}
 
-        parent::__construct( $out, $in );
-    }
+	/**
+	 * Setter for RegExp
+	 * @param string $re The regex
+	 */
+	public function setRegExp( $re ) {
+		$this->re = $re;
+	}
 
-    function setRegExp( $re ) {
-        $this->re = $re;
-    }
+	/**
+	 * Setter for the target NS
+	 * @param string $ns The namespace
+	 */
+	public function setNewNS( $ns ) {
+		$this->ns = $ns;
+	}
 
-    function setNewNS( $ns ) {
-        $this->ns = $ns;
-    }
+	/**
+	 * Setter for target NS ID
+	 * @param int $nsID Namespace ID
+	 */
+	public function setNewNSID( $nsID ) {
+		$this->nsID = $nsID;
+	}
 
-    function setNewNSID( $nsID ) {
-        $this->nsID = $nsID;
-    }
+	/**
+	 * Internal method to get the old NS
+	 */
+	protected function getOldCase() {
+		$this->oldCase = $this->current()->readInnerXML();
+		do {
+			$this->write();
+			$this->next();
+		} while ( $this->current()->nodeType !== XMLReader::END_ELEMENT );
+	}
 
-    protected function getOldCase() {
-        $this->oldCase = $this->current()->readInnerXML();
-        do {
-            $this->write();
-            $this->next();
-        } while ( $this->current()->nodeType !== XMLReader::END_ELEMENT );
-    }
+	/**
+	 * Internal method to get all the namespaces used.
+	 */
+	protected function findNamespaces() {
+		$current = $this->current();
+		if ( $current === null ) {
+			$this->next();
+			$current = $this->current();
+		}
+		while ( $current && $current->name !== "namespaces" ) {
+			if ( $current->name === "case" ) {
+				$this->getOldCase();
+			}
+			$this->write();
+			$this->next();
+			$current = $this->current();
+		}
+		$this->write();
+		$this->next();
+	}
 
-    protected function findNamespaces() {
-        $current = $this->current();
-        if ( $current === null ) {
-            $this->next();
-            $current = $this->current();
-        }
-        while ( $current && $current->name !== "namespaces" ) {
-            if ( $current->name === "case" ) {
-                $this->getOldCase();
-            }
-            $this->write();
-            $this->next();
-            $current = $this->current();
-        }
-        $this->write();
-        $this->next();
-    }
+	/**
+	 * Internal method to add those namespaces being copied to the output file
+	 */
+	protected function addNewNamespace() {
+		echo "Inserting new namespace\n";
+		$this->writer->text( "\n      " );
+		$this->writer->startElement( "namespace" );
+		$this->writer->writeAttribute( "key", $this->nsID );
+		$this->writer->writeAttribute( "case", $this->oldCase );
+		$this->writer->text( $this->ns );
+		$this->writer->endElement();
+		$this->writer->text( "\n      " );
+		do {
+			$this->next();
+			$this->write();
+			if ( $this->current()->name === "namespaces"
+				 && $this->current()->nodeType === XMLReader::END_ELEMENT ) {
+				break;
+			}
+		} while ( 1 );
+		$this->next();
+	}
 
-    protected function addNewNamespace() {
-        echo "Inserting new namespace\n";
-        $this->writer->text( "\n      " );
-        $this->writer->startElement( "namespace" );
-        $this->writer->writeAttribute( "key", $this->nsID );
-        $this->writer->writeAttribute( "case", $this->oldCase );
-        $this->writer->text( $this->ns );
-        $this->writer->endElement();
-        $this->writer->text( "\n      " );
-        do {
-            $this->next();
-            $this->write();
-            if ( $this->current()->name === "namespaces"
-                 && $this->current()->nodeType === XMLReader::END_ELEMENT ) {
-                break;
-            }
-        } while(1);
-        $this->next();
-   }
+	/**
+	 * Internal method to skip ahead to the pages
+	 */
+	protected function skipToPages() {
+		do {
+			$this->write();
+			$this->next();
+		} while ( $this->current()->name !== "page" );
+	}
 
-    protected function skipToPages() {
-        do {
-            $this->write();
-            $this->next();
-        } while ( $this->current()->name !== "page" );
-    }
+	/**
+	 * Main entry point that does all the heavy lifting
+	 * @param Maintenance $maint Object that we'll use for user notifications
+	 */
+	public function transform( Maintenance $maint ) {
+		$this->oldCase = "first-letter";
+		$this->findNamespaces();
+		$this->addNewNamespace();
+		$this->skipToPages();
 
-    function transform() {
-        $this->oldCase = "first-letter";
-        $this->findNamespaces();
-        $this->addNewNamespace();
-        $this->skipToPages();
+		do {
+			$xml = $this->filter( $maint );
+			if ( $xml instanceof DomDocument ) {
+				$this->append( $xml );
+			}
+		} while ( $this->reader-> next() );
+	}
 
-        do {
-            $xml = $this->filter();
-            if ( $xml instanceOf DOMNode ) {
-                $this->append( $xml );
-            }
-        } while ( $this->reader-> next() );
-    }
+	/**
+	 * Internal method to provide parsed elements of a page's representation
+	 * @return array
+	 *    0 - DomElement for Title
+	 *    1 - DomElement for Namespace
+	 *    2 - DomElement for NamespaceID
+	 *    3 - DomElement for text
+	 *    4 - DomDocument for this page
+	 * @FIXME yes, this should be its own class
+	 */
+	protected function getPageElements() {
+		// dummy doc so we can deal with DomDocument
+		$dom = new DomDocument;
 
-    protected function getSXE() {
-        // dummy doc so we can deal with DomDocument
-        $dom = new DomDocument;
+		// Add input to dummy doc
+		$dom->appendChild( $this->reader->expand() );
 
-        // Add input to dummy doc
-        $dom->appendChild($this->reader->expand());
+		// do xpath query for this guy
+		$xpath = new DOMXPath( $dom );
+		$xpath->registerNamespace( "m", "http://www.mediawiki.org/xml/export-0.10/" );
+		$title = $xpath->evaluate( "/m:page/m:title" );
+		$ns = $xpath->evaluate( "/m:page/m:ns" );
+		$id = $xpath->evaluate( "/m:page/m:id" );
+		$text = $xpath->evaluate( "/m:page/m:revision/m:text" );
 
-        // do xpath query for this guy
-        $xpath = new DOMXPath( $dom );
-        $xpath->registerNamespace( "m", "http://www.mediawiki.org/xml/export-0.10/" );
-        $title = $xpath->evaluate("/m:page/m:title");
-        $ns = $xpath->evaluate("/m:page/m:ns");
-        $id = $xpath->evaluate("/m:page/m:id");
-        $text = $xpath->evaluate("/m:page/m:revision/m:text");
+		return [ $title[0], $ns[0], $id[0], $text, $dom ];
+	}
 
-        return [ $title[0], $ns[0], $id[0], $text, $dom ];
-    }
+	/**
+	 * Method to handle the conversion of a page
+	 * @param Maintenance $maint object for user feedback.
+	 * @return DomDocument of massaged page
+	 */
+	public function filter( Maintenance $maint ) {
+		list( $titleEL, $nsEL, $idEL, $textEL, $xml ) = $this->getSXE();
+		$append = false;
+		if ( $titleEL ) {
+			$title = $titleEL->textContent;
+			$id = int( $idEL->textContent );
+			$newNS = $this->ns;
 
-    function filter() {
-        list($titleEL, $nsEL, $idEL, $textEL, $xml) = $this->getSXE();
-        $append = false;
-        if ( $titleEL ) {
-            $title = $titleEL->textContent;
-            if ( !preg_match( "/{$this->re}/", $title ) ) {
-                if ( substr( $title, 0, 5 ) === "Talk:" ) {
-                    $title = $this->ns . ' talk:' . substr( $title, 5 );
-                } elseif ( substr( $title, 0, 9 ) !== "Category:" ) {
-                    $title = $this->ns . ":$title";
-                }
-                $titleEL->textContent = $title;
+			if ( $id == 1 ) {
+				$newNS = "Talk";
+			}
 
-                $nsEL->textContent = $this->nsID + $nsEL->textContent;
-                $idEL->textContent = '';
-                $this->fixLinks( $textEL );
+			if ( int( $id ) > 1 ) {
+				$match = [];
 
-                $append = $xml;
-            }
+				# Namespaces don't have : in them, right?
+				if ( !preg_match( "([^:])+:", $title, $match ) ) {
+					$maint->error( wfMessage( "importw2ns-ns-match", $title )->plain() );
+					return false;
+				}
+				$newNS = $match[1];
+			}
 
-        }
+			if ( substr( $title, 0, 5 ) === "Talk:" ) {
+				$title = $this->ns . ' talk:' . substr( $title, 5 );
+			} elseif ( substr( $title, 0, 9 ) !== "Category:" ) {
+				$title = $this->ns . ":$title";
+			}
+			$titleEL->textContent = $title;
 
-        return $append;
-    }
+			$nsEL->textContent = $this->nsID + $nsEL->textContent;
+			$idEL->textContent = '';
+			$this->fixRevisions( $textEL );
 
-    function fixLinks( $textEL ) {
-        $preTitleRE = '#\[\[';
-        $postTitleRE = '#';
-        foreach ( $textEL as $revision ) {
-            $revText = $revision->textContent;
-            preg_match_all( $preTitleRE . '([^|\]]+)' . $postTitleRE, $revText, $match );
-            $links = array_unique( $match[1] );
-            foreach ( $links as $link ) {
-                $update = $this->shouldUpdate( $link );
-                if ( $update ) {
-                    $revText = preg_replace(
-                        $preTitleRE . $link . $postTitleRE, '[[' . $update, $revText
-                    );
-                }
-            }
+			$append = $xml;
+		}
+		return $append;
+	}
 
-            preg_replace( "#<sha1>[^<]+</sha1>#", '', $revText );
-            $revision->textContent = $revText;
-        }
-    }
+	/**
+	 * Fix up page all revisions of page content
+	 * @param DomElement $textEL the revisions
+	 */
+	protected function fixRevisions( $textEL ) {
+		$preTitleRE = '#\[\[';
+		$postTitleRE = '#';
+		foreach ( $textEL as $revision ) {
+			$revText = $revision->textContent;
+			preg_match_all( $preTitleRE . '([^|\]]+)' . $postTitleRE, $revText, $match );
+			$links = array_unique( $match[1] );
+			foreach ( $links as $link ) {
+				$update = $this->shouldUpdate( $link );
+				if ( $update ) {
+					$revText = preg_replace(
+						$preTitleRE . $link . $postTitleRE, '[[' . $update, $revText
+					);
+				}
+			}
 
-    protected function shouldUpdate( $link ) {
-        if ( isset( $this->titles[ lcFirst( $link ) ] )
-             && $this->titles[ lcFirst( $link ) ] ) {
-            return $this->ns . ':' . $link;
-        }
-    }
+			preg_replace( "#<sha1>[^<]+</sha1>#", '', $revText );
+			$revision->textContent = $revText;
+		}
+	}
 
-    protected function xmlWithoutPI( $xml ) {
-        $str = $xml->saveXML();
-        $pattern = '~
+	/**
+	 * Determine if this title should be updated
+	 * @param string $link link text to update
+	 * @return null|string if fixed
+	 */
+	protected function shouldUpdate( $link ) {
+		if ( isset( $this->titles[ lcFirst( $link ) ] )
+			 && $this->titles[ lcFirst( $link ) ] ) {
+			return $this->ns . ':' . $link;
+		}
+	}
+
+	/**
+	 * Strip the starting (and any other) PI from an XML string
+	 * @param DomDocument $xml to clean
+	 * @return string
+	 */
+	protected function xmlWithoutPI( DomDoument $xml ) {
+		$str = $xml->saveXML();
+		$pattern = '~
 <\?
-    (?: [A-Za-z_:] | [^\x00-\x7F] ) (?: [A-Za-z_:.-] | [^\x00-\x7F] )*
-    (?: \?> | \s (?: [^?]* \?+ ) (?: [^>?] [^?]* \?+ )* >)
+	(?: [A-Za-z_:] | [^\x00-\x7F] ) (?: [A-Za-z_:.-] | [^\x00-\x7F] )*
+	(?: \?> | \s (?: [^?]* \?+ ) (?: [^>?] [^?]* \?+ )* >)
 ~x';
 
-        return preg_replace($pattern, '', $str);
-    }
+		return preg_replace( $pattern, '', $str );
+	}
 
-    function append( $xml ) {
-        if ( !method_exists( $xml, "saveXML" ) ) {
-            $dom = new DomDocument;
-            $dom->appendChild($xml);
-            $xml = $dom;
-        }
+	/**
+	 * Append the xml fragment to the output
+	 * @param DomDocument $xml document
+	 */
+	protected function append( DomDocument $xml ) {
+		$str = $this->xmlWithoutPI( $xml );
+		$this->writer->writeRaw( $str );
+		$this->writer->flush();
+	}
 
-        $str = $this->xmlWithoutPI( $xml );
-        $this->writer->writeRaw( $str );
-        $this->writer->flush();
-    }
+	/**
+	 * Skip over the bits we don't want till we get to the XML node we want
+	 * @param XmlReader $in the input
+	 * @param string $elName what we're looking for
+	 * @return bool whether to continue or not
+	 */
+	protected function readUntil( XmlReader $in, $elName ) {
+		$continue = true;
+		do {
+			$continue = $in->read();
+		} while ( $continue && $in->name !== $elName
+				 || ( $in->nodeType !== XMLReader::ELEMENT
+					  && $in->name === $elName ) );
+		return $continue;
+	}
 
-    protected function readUntil( $in, $elName ) {
-        $continue = true;
-        do {
-            $continue = $in->read();
-        } while( $continue && $in->name !== $elName
-                 || ( $in->nodeType !== XMLReader::ELEMENT
-                      && $in->name === $elName ) );
-        return $continue;
-    }
+	/**
+	 * Get the page titles from this the input
+	 * @param XmlReader $in the input
+	 */
+	protected function getPageTitles( XmlReader $in ) {
+		$this->readUntil( $in, "siteinfo" );
+		// Get down on the same level as pages
+		$continue = $this->readUntil( $in, "page" );
+		while ( $continue ) {
+			$this->readUntil( $in, "title" );
+			$title = $in->readInnerXML();
 
-    function getPageTitles( $in ) {
-        $this->readUntil( $in, "siteinfo" );
-        // Get down on the same level as pages
-        while ( $page = $this->readUntil( $in, "page" ) ) {
-            $this->readUntil( $in, "title" );
-            $title = $in->readInnerXML();
+			$this->readUntil( $in, "ns" );
+			$ns = $in->readInnerXML();
 
-            $this->readUntil( $in, "ns" );
-            $ns = $in->readInnerXML();
-
-            // Only copy the main namespace
-            if ( $ns < 2 ) {
-                $this->titles[lcFirst( $title )] = true;
-            }
-        }
-    }
+			// Only copy the main namespace
+			if ( $ns < 2 ) {
+				$this->titles[lcFirst( $title )] = true;
+			}
+			$continue = $this->readUntil( $in, "page" );
+		}
+	}
 }
